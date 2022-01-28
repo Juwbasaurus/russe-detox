a989cae8bf61459877f2713b31c9a58bcbde37f7
Jewbasaurus <loferist@gmail.com>
2022-01-28 20:19:55 +0300

    [ref] inference function

--- infer.py
+++ infer.py
@@ -1,47 +1,24 @@
-import csv
-
 from transformers import (
     GPT2LMHeadModel,
     GPT2TokenizerFast,
 )
 
-tokenizer = GPT2TokenizerFast.from_pretrained('models/test')
-model = GPT2LMHeadModel.from_pretrained('models/test')
 
-with open('data/orig/input/dev.tsv', 'r', encoding='utf8') as f:
-    reader = csv.reader(f, delimiter='\t')
-    next(reader)
-    i = 0
-    for row in reader:
-        print(row[0])
-        input = tokenizer.encode(row[0], return_tensors='pt')
-        model_output = model.generate(
-            input,
-            do_sample=True,
-            max_length=256,
-            top_k=40,
-            top_n=0.7,
-            temperature=0.95,
-            num_return_sequences=1,
-            early_stopping=True,
-            eos_token_id=tokenizer.eos_token_id,
-        )
-        for out in model_output:
-            print(tokenizer.decode(out))
-        if i >= 10:
-            break
-        else:
-            i += 1
-
-        # input = tokenizer.encode(row[0] + '===', return_tensors='pt')
-        # model_output = model.generate(
-        #     input,
-        #     max_length=128,
-        #     num_beams=5,
-        #     no_repeat_ngram_size=3,
-        #     num_return_sequences=1,
-        #     early_stopping=True,
-        #     eos_token_id=tokenizer.eos_token_id,
-        # )
-        # for out in model_output:
-        #     print(tokenizer.decode(out))
+def infer(
+    model: GPT2LMHeadModel,
+    tokenizer: GPT2TokenizerFast,
+    text: str,
+) -> str:
+    input = tokenizer.encode(text, return_tensors='pt')
+    model_output = model.generate(
+        input,
+        do_sample=True,
+        max_length=256,
+        top_k=40,
+        top_n=0.7,
+        temperature=0.95,
+        num_return_sequences=1,
+        early_stopping=True,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    return tokenizer.decode(model_output)
---
+++ inference.py
@@ -0,0 +1,49 @@
+import csv
+
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2TokenizerFast,
+)
+
+from infer import infer
+
+tokenizer = GPT2TokenizerFast.from_pretrained('models/test')
+model = GPT2LMHeadModel.from_pretrained('models/test')
+
+with open('data/orig/input/dev.tsv', 'r', encoding='utf8') as f:
+    reader = csv.reader(f, delimiter='\t')
+    next(reader)
+    i = 0
+    for row in reader:
+        print(row[0])
+        input = tokenizer.encode(row[0], return_tensors='pt')
+        model_output = model.generate(
+            input,
+            do_sample=True,
+            max_length=256,
+            top_k=40,
+            top_n=0.7,
+            temperature=0.95,
+            num_return_sequences=1,
+            early_stopping=True,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+        for out in model_output:
+            print(tokenizer.decode(out))
+        if i >= 10:
+            break
+        else:
+            i += 1
+
+        # input = tokenizer.encode(row[0] + '===', return_tensors='pt')
+        # model_output = model.generate(
+        #     input,
+        #     max_length=128,
+        #     num_beams=5,
+        #     no_repeat_ngram_size=3,
+        #     num_return_sequences=1,
+        #     early_stopping=True,
+        #     eos_token_id=tokenizer.eos_token_id,
+        # )
+        # for out in model_output:
+        #     print(tokenizer.decode(out))
